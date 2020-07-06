14c905da16841bb6763bd1116f044177bb07c828
Ezequiel <ezequiel3434@gmail.com>
2020-07-06 10:43:06 -0300

    Initial Commit

---
+++ Proyecto1-StormViwer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proyecto1-StormViwer
+//
+//  Created by Ezequiel Parada Beltran on 06/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
