ca0dba812e588d64664b28e53a3613d58b4038a6
Ezequiel <ezequiel3434@gmail.com>
2020-07-07 17:11:58 -0300

    hidesBarsOnTap and large titles

--- Proyecto1-StormViwer/ViewController.swift
+++ Proyecto1-StormViwer/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Storm Viwer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -39,6 +42,13 @@ class ViewController: UITableViewController {
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
 }
 
